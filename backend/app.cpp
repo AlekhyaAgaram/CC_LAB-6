ac2cabe9ecc27d0a9c70131d91eb4ffe4d724ed0
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-02-26 14:18:24 +0530

    Clean Lab 6 setup

---
+++ backend/app.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <cstring>
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    char hostname[1024];
+    gethostname(hostname, 1024);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while (true) {
+        new_socket = accept(server_fd,
+                            (struct sockaddr *)&address,
+                            (socklen_t*)&addrlen);
+
+        std::string body =
+            "Served by backend: " + std::string(hostname);
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: " + std::to_string(body.size()) + "\r\n"
+            "\r\n" + body;
+
+        send(new_socket, response.c_str(), response.size(), 0);
+        close(new_socket);
+    }
+}
